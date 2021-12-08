ffc52c0c4c4e3227305ddc3ccab6841f2ac65bae
andreharasym <91905082+andreharasym@users.noreply.github.com>
2021-12-08 21:25:24 +0300

    Add files via upload

---
+++ client.py
@@ -0,0 +1,39 @@
+# client.py
+import socket
+import threading
+
+
+nickname = input("Choose your nickname : ").strip()
+while not nickname:
+    nickname = input("Your nickname should not be empty : ").strip()
+my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = "192.168.55.121"
+port = 5555
+my_socket.connect((host, port))
+
+
+def thread_sending():
+    while True:
+        message_to_send = input()
+        if message_to_send:
+            message_with_nickname = nickname + " : " + message_to_send
+            # cezar.myencode(message_with_nickname)
+            my_socket.send(bytes(message_with_nickname, 'utf-8'))
+
+
+def thread_receiving():
+    # import cezar
+    while True:
+        message = my_socket.recv(1024)
+        # cezar.mydecode(message)                     #дишифруєм повідомлення що отримуєм
+        print(message.decode())
+
+
+thread_send = threading.Thread(target=thread_sending)
+thread_receive = threading.Thread(target=thread_receiving)
+thread_send.start()
+thread_receive.start()
+
+
+
+
